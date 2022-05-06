3b7d8753f64a10c252438c85e80f0c064302342b
cpatterson23 <ceddypbody@gmail.com>
2022-05-05 19:28:21 -0500

    discord weather bot

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,28 @@
 import * as Discord from 'discord.js';
-import { getTemperature } from './weather';
+import { getTemperature } from './weather.js';
 
-const token = "OTY5MzgwNzI3MTk4MjA4MDAw.YmskJw.5i6nGYzpyoV1GQvMw1yMUBlieZk";
+const token = "";
 const client = new Discord.Client({
     intents: ['GUILDS', 'GUILD_MESSAGES'],
 });
 
 async function onMessage(message) {
+    // Get the numbers out of message.content
+    // message.content is a string
+
+    let numbers = (message.content.match(/\d+/));
+    let zipcode = numbers[0]
     const temp = await getTemperature(zipcode);
     message.reply(`heres the temperature: ${temp}*f`);
+    
+
+    console.log(numbers);
+}
+
+async function onReady() {
+    console.log('the bot is ready');
 }
 
 client.on("messageCreate", onMessage);
-client.on("ready", onReady)
+client.on("ready", onReady);
 client.login(token);
